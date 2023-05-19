b339ed937c012490a9c429cd2f398050e0d1ed5a
karkigrishmin <grishminkarki7@gmail.com>
2023-05-19 16:34:24 +0545

    implement accordion and populate fetched data in ui

--- src/feature/permissions/ui/permission.tsx
+++ src/feature/permissions/ui/permission.tsx
@@ -1,190 +1,176 @@
 import { AiOutlineClose } from "react-icons/ai";
 import { FiChevronRight } from "react-icons/fi";
 import "./permission.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { IPermssionInfo, getPermissionData } from "..";
 
 export const Permission = () => {
-	const [isAccordionItemVisible, setAccordionItemVisibility] = useState(false);
+	const [activeAccordions, setActiveAccordions] = useState<number[]>([]);
+	const [permissionData, setPermissionData] = useState<IPermssionInfo | null>(
+		null
+	);
+	const [editStatus, setEditStatus] = useState(false);
 
-	const handleAccordionHideShow = () => {
+	const handleAccordionState = (moduleId: number) => {
 		console.log("handleAccordionHideShow");
-		setAccordionItemVisibility(!isAccordionItemVisible);
+		if (activeAccordions.includes(moduleId)) {
+			setActiveAccordions(
+				activeAccordions.filter(
+					(eachActiveAccordion) => eachActiveAccordion !== moduleId
+				)
+			);
+		} else {
+			setActiveAccordions((prevActiveAccordions) => [
+				...prevActiveAccordions,
+				moduleId,
+			]);
+		}
 	};
 
-	console.log("isAccordionItemVisible", isAccordionItemVisible);
+	console.log("active accordions", activeAccordions);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await getPermissionData();
+				setPermissionData(data);
+			} catch (error) {
+				console.log("fetchData error", error);
+			}
+		};
+
+		fetchData();
+	}, []);
 
 	return (
 		<div className='permission-wrapper'>
 			<div className='permission-header'>
 				<div className='permission-header__left-item'>
-					<p>Edit Permission</p>
-					<p>(Shreel Adhikari)</p>
+					<p>{editStatus ? "Edit Permission" : "Permission"}</p>
+					{editStatus && <p>({permissionData?.name ?? ""})</p>}
 				</div>
 
 				<div className='permission-header__right-item'>
 					<div className='permission-actions'>
-						<button>Save</button>
-						<div className='close-icon'>
-							<AiOutlineClose />
-						</div>
+						<button
+							onClick={() => {
+								setEditStatus(!editStatus);
+							}}
+						>
+							{editStatus ? "Save" : "Edit"}
+						</button>
+						{editStatus && (
+							<div className='close-icon'>
+								<AiOutlineClose
+									onClick={() => {
+										setEditStatus(!editStatus);
+									}}
+								/>
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
 
 			<main className='modules'>
 				<div className='modules__wrapper'>
-					<section className='accordion'>
-						<div
-							className={`accordion__title ${
-								isAccordionItemVisible ? "selected" : ""
-							}`}
-						>
-							<div onClick={handleAccordionHideShow}>
-								<p>Setting</p>
-								<div className='chevron-icon'>
-									<FiChevronRight />
+					{permissionData?.modules.map((eachModule) => (
+						<section className='accordion'>
+							<div
+								className={`accordion__title ${
+									activeAccordions.includes(eachModule.m_id) ? "selected" : ""
+								}`}
+							>
+								<div onClick={() => handleAccordionState(eachModule.m_id)}>
+									<p>{eachModule.m_name}</p>
+									<div className='chevron-icon'>
+										<FiChevronRight />
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<table className={`${!isAccordionItemVisible ? "hide" : ""}`}>
-							<thead>
-								<tr>
-									<th>
-										<div>
-											<input
-												type='checkbox'
-												name='settingPermission'
-												id='settingPermission'
-												className='myCheckboxClass'
-											/>
-											<label
-												onClick={() =>
-													console.log("checkbox setting permissionS")
-												}
-											></label>
-										</div>
-									</th>
-									<th>Setting Permission</th>
-									<th>View</th>
-									<th>Edit</th>
-								</tr>
-							</thead>
+							<table
+								className={`${
+									activeAccordions.includes(eachModule.m_id) ? "show" : "hide"
+								}
+							
+									`}
+							>
+								<thead>
+									<tr>
+										<th>
+											<div>
+												<input
+													type='checkbox'
+													name='settingPermission'
+													id='settingPermission'
+													className='myCheckboxClass'
+												/>
+												<label
+													onClick={() =>
+														console.log("checkbox setting permissionS")
+													}
+												></label>
+											</div>
+										</th>
+										<th>{`${eachModule.m_name}  Permission`.toUpperCase()}</th>
+										{eachModule.columns.map(
+											(eachColumn, index) =>
+												index === 0 &&
+												eachColumn.c_permissions.map(
+													(eachPermission, index) => (
+														<th>{eachPermission.p_name}</th>
+													)
+												)
+										)}
+									</tr>
+								</thead>
 
-							<tbody>
-								<tr>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='organization'
-												id='organization'
-												className='myCheckboxClass'
-											/>
-											<label></label>
-										</div>
-									</td>
-									<td>Organization</td>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='view'
-												id='view'
-												className='myCheckboxClass'
-											/>
-											<label></label>
-										</div>
-									</td>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='edit'
-												id='edit'
-												className='myCheckboxClass'
-												checked
-											/>
-											<label></label>
-										</div>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='posSetting'
-												id='posSetting'
-												className='myCheckboxClass'
-											/>
-											<label></label>
-										</div>
-									</td>
-									<td>POS Setting</td>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='view'
-												id='view'
-												className='myCheckboxClass'
-												checked
-											/>
-											<label></label>
-										</div>
-									</td>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='edit'
-												id='edit'
-												className='myCheckboxClass'
-											/>
-											<label></label>
-										</div>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='userAndPermissions'
-												id='userAndPermissions'
-												className='myCheckboxClass'
-											/>
-											<label></label>
-										</div>
-									</td>
-									<td>User and Permissions</td>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='view'
-												id='view'
-												className='myCheckboxClass'
-											/>
-											<label></label>
-										</div>
-									</td>
-									<td>
-										<div>
-											<input
-												type='checkbox'
-												name='edit'
-												id='edit'
-												className='myCheckboxClass'
-											/>
-											<label></label>
-										</div>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</section>
+								<tbody>
+									{eachModule.columns.map((eachColumn) => (
+										<tr>
+											<td>
+												<div>
+													<input
+														type='checkbox'
+														name={eachColumn.c_name}
+														id={eachColumn.c_id.toString()}
+														className='myCheckboxClass'
+														checked={eachColumn.c_status}
+													/>
+													<label
+														onClick={() => {
+															console.log(
+																"module id: ",
+																eachModule.m_id,
+																"column id: ",
+																eachColumn.c_id
+															);
+														}}
+													></label>
+												</div>
+											</td>
+											<td>{eachColumn.c_name}</td>
+											{eachColumn.c_permissions.map((eachPermission) => (
+												<td>
+													<div>
+														<input
+															type='checkbox'
+															name={eachPermission.p_name}
+															id={eachPermission.p_name}
+															className='myCheckboxClass'
+															checked={eachPermission.p_status}
+														/>
+														<label></label>
+													</div>
+												</td>
+											))}
+										</tr>
+									))}
+								</tbody>
+							</table>
+						</section>
+					))}
 				</div>
 			</main>
 		</div>
