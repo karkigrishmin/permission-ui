b8538a9125db6f9996e29bf9366534349c5f2938
karkigrishmin <grishminkarki7@gmail.com>
2023-05-19 10:58:17 +0545

    create navbar

--- src/shared/ui/components/navbar/navbar.tsx
+++ src/shared/ui/components/navbar/navbar.tsx
@@ -1,3 +1,35 @@
+import "./navbar.css";
+import { AiFillSetting } from "react-icons/ai";
+import { GrNotification } from "react-icons/gr";
+import { BiHelpCircle } from "react-icons/bi";
+
 export const NavBar = () => {
-	return <>Navbar</>;
+	return (
+		<header>
+			<nav>
+				<a className='logo' href='/'>
+					<span>tigg</span>
+					<span>POS</span>
+				</a>
+
+				<div className='navbar-right-icons'>
+					<div>
+						<AiFillSetting />
+					</div>
+
+					<div>
+						<GrNotification className='notification-icon' />
+					</div>
+
+					<div>
+						<BiHelpCircle />
+					</div>
+
+					<div className='user-avatar'>
+						<img src='src/assets/images/messi.png' alt='avatar' />
+					</div>
+				</div>
+			</nav>
+		</header>
+	);
 };
