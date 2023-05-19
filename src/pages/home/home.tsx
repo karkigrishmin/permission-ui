a5ee50dc0919702c21d16281acb6deadbae03985
karkigrishmin <grishminkarki7@gmail.com>
2023-05-19 11:00:51 +0545

    render navbar and permission component  in homepage

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,14 @@
+import { Permission } from "../../feature";
 import { NavBar } from "../../shared";
+import "./home.css";
 
 export const Home = () => {
 	return (
-		<>
+		<div className='home'>
 			<NavBar />
-		</>
+			<main>
+				<Permission />
+			</main>
+		</div>
 	);
 };
